3f745b27e468d93eda1ffc4d1d712c12fc362727
owenstranathan <owenstranathan@gmail.com>
2015-05-21 14:32:28 -0400

    Got some stuff working

--- src/test/floatCanvasTest.py
+++ src/test/floatCanvasTest.py
@@ -6,6 +6,9 @@ SCREEN_HEIGHT = 640
 SCREEN_WIDTH = 800
 grid_cell_size = 30
 
+def YDownProjection(CenterPoint):
+    return N.array((1,-1))
+
 class Example(wx.Frame):
     def __init__(self, parent, id, title, position, size):
         wx.Frame.__init__(self, parent, id, title, position, size)
@@ -15,9 +18,11 @@ class Example(wx.Frame):
         self.Canvas.Bind(FloatCanvas.EVT_MOTION, self.onMouseMove)
         self.Canvas.Bind(FloatCanvas.EVT_MOUSEWHEEL, self.onWheel)
         ##BIND KEYBOARD EVENTS
-        #self.Canvas.Bind(FloatCanvas.EVT_KEY_DOWN, self.onKeyDown)
+        self.Canvas.Bind(wx.EVT_KEY_DOWN, self.onKeyDown)
 
         self.firstClick = False
+        self.lines = []
+
 
     ##INIT UI AND FRAME_SETTINGS
     def InitUI(self):
@@ -30,11 +35,11 @@ class Example(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnQuit, fitem)
         self.Canvas = NavCanvas.NavCanvas(self, -1,
                              size=(500, 500),
-                             ProjectionFun=None,
+                             ProjectionFun=YDownProjection,
                              Debug=0,
                              BackgroundColor="White",
                              ).Canvas
-
+        self.Canvas.InitAll()
         self.SetSize((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.SetTitle('Simple menu')
         self.Centre()
@@ -48,36 +53,45 @@ class Example(wx.Frame):
     def onLeftDown(self, event):
         if self.firstClick is False:
             self.firstClick = True
-            self.pos = self.getSnapPos(event.GetPositionTuple())
+            self.pos = self.getSnapPos(event.GetCoords())
+
             print self.pos
         else:
-            self.lines.append(self.pos + self.getSnapPos(event.GetPositionTuple()))
+            self.lines.append((self.pos , self.getSnapPos(event.GetCoords())))
             if len(self.lines) != 0:
-               self.pos = self.getSnapPos(event.GetPositionTuple())
+               self.pos = self.getSnapPos(event.GetCoords())
 
             event.Skip()
 
     def onKeyDown(self, event):
-        pass
+        key_code = event.GetKeyCode()
+        if key_code == wx.WXK_ESCAPE:
+           self.firstClick = False
+
     ##POSSIBLY DEPRECIATED
     def getSnapPos(self, arg_pos):
         return (grid_cell_size* round(arg_pos[0]/grid_cell_size), grid_cell_size*round(arg_pos[1]/grid_cell_size))
 
     def onMouseMove(self,event):
-        # if event.Moving() and self.firstClick:
-        #     self.drawMotion(event)
-        #     event.Skip()
-        pass
+        if event.Moving() and self.firstClick:
+            self.drawMotion(event)
+        event.Skip()
+
 
     def drawMotion(self, event):
-
-        # self.newPos = self.getSnapPos(event.GetPositionTuple())
-        # coords = self.pos + self.newPos
-        # self.Canvas.AddArrowLine(*coords, LineWidth=2, LineColor='BLUE', ArrowHeadSize=8)
-        pass
+        self.newPos = self.getSnapPos(event.GetCoords())
+        coords = (self.pos , self.newPos)
+        print coords
+        self.Canvas.AddArrowLine(coords, LineWidth=2, LineColor='BLUE', ArrowHeadSize=8)
+        #self.Canvas.ZoomToBB()
+        self.Canvas.Draw()
+        self.Canvas.InitAll()
+        for line in self.lines:
+            self.Canvas.AddArrowLine(line, LineWidth=2, LineColor="GREEN", ArrowHeadSize=8  )
 
     def onWheel(self,event):
         pass
+
 def main():
 
     ex = wx.App()

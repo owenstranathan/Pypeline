8b7d70b1e0fa7352adf400547c7d7e6ab9c14489
nimsec <ian.stranathan91@gmail.com>
2015-05-27 19:16:41 +0300

    Update floatCanvasTest.py
    sub classed arrowhead directly in file, changed code to add object to canvas instead of using AddXYZ syntax

--- src/test/floatCanvasTest.py
+++ src/test/floatCanvasTest.py
@@ -9,9 +9,89 @@ from wx.lib.floatcanvas import NavCanvas, FloatCanvas
 SCREEN_HEIGHT = 640
 SCREEN_WIDTH = 800
 grid_cell_size = 30
-
+#
 def YDownProjection(CenterPoint):
-    return N.array((1,-1))
+    return N.array((1, -1))
+
+class SmoothArrowLine(FloatCanvas.PointsObjectMixin, FloatCanvas.LineOnlyMixin, FloatCanvas.DrawObject):
+    """
+    ArrowLine class definition.
+
+    API definition::
+
+        ArrowLine(Points, # coords of points
+                  LineColor = "Black",
+                  LineStyle = "Solid",
+                  LineWidth    = 1,
+                  ArrowHeadSize = 4, # in pixels
+                  ArrowHeadAngle = 45,
+                  InForeground = False):
+
+
+    It will draw a set of arrows from point to point.
+
+    It takes a list of 2-tuples, or a NX2 NumPy Float array
+    of point coordinates.
+
+
+    """
+
+    def __init__(self,
+                 Points,
+                 LineColor = "Black",
+                 LineStyle = "Solid",
+                 LineWidth    = 1, # pixels
+                 ArrowHeadSize = 8, # pixels
+                 ArrowHeadAngle = 30, # degrees
+                 InForeground = False):
+
+        FloatCanvas.DrawObject.__init__(self, InForeground)
+
+        self.Points = N.asarray(Points,N.float)
+        self.Points.shape = (-1,2) # Make sure it is a NX2 array, even if there is only one point
+        self.ArrowHeadSize = ArrowHeadSize
+        self.ArrowHeadAngle = float(ArrowHeadAngle)
+
+        self.CalcArrowPoints()
+        self.CalcBoundingBox()
+
+        self.LineColor = LineColor
+        self.LineStyle = LineStyle
+        self.LineWidth = LineWidth
+
+        self.SetPen(LineColor,LineStyle,LineWidth)
+
+        self.HitLineWidth = max(LineWidth,self.MinHitLineWidth)
+
+    def CalcArrowPoints(self):
+        S = self.ArrowHeadSize
+        phi = self.ArrowHeadAngle * N.pi / 360
+        Points = self.Points
+        n = Points.shape[0]
+        self.ArrowPoints = N.zeros((n-1, 3, 2), N.float)
+        for i in xrange(n-1):
+            dx, dy = self.Points[i] - self.Points[i+1]
+            theta = N.arctan2(dy, dx)
+            AP = N.array( (
+                            (N.cos(theta - phi), -N.sin(theta-phi)),
+                            (0,0),
+                            (N.cos(theta + phi), -N.sin(theta + phi))
+                            ),
+                          N.float )
+            self.ArrowPoints[i,:,:] = AP
+        self.ArrowPoints *= S
+
+    def _Draw(self, dc , WorldToPixel, ScaleWorldToPixel, HTdc=None):
+        Points = WorldToPixel(self.Points)
+        ArrowPoints = Points[1:,N.newaxis,:] + self.ArrowPoints
+        dc.SetPen(self.Pen)
+        dc.DrawLines(Points)
+        GC = wx.GraphicsContext.Create(dc)
+        GC.SetPen(self.Pen)
+        GC.DrawLines(Points)
+        for arrow in ArrowPoints:
+                GC.DrawLines(arrow)
+
 
 class Example(wx.Frame):
     def __init__(self, parent, id, title, position, size):
@@ -28,6 +108,8 @@ class Example(wx.Frame):
         self.firstClick = False
         self.lines = []
         self.node_pos_list = []
+        self.redo_lines = []
+        self.redo_nodes = []
 
 
     ##INIT UI AND FRAME_SETTINGS
@@ -71,13 +153,20 @@ class Example(wx.Frame):
             self.lines.append((self.pos, self.getSnapPos(event.GetCoords())))
             if len(self.lines) != 0:
                 self.pos = self.getSnapPos(event.GetCoords())
+                print self.pos
                 self.node_pos_list.append(self.pos)
         event.Skip()
 
     def onKeyDown(self, event):
+
         key_code = event.GetKeyCode()
         if key_code == wx.WXK_ESCAPE:
            self.firstClick = False
+           print self.lines
+           print self.node_pos_list
+        #elif key_code == wx.WXK_TAB:
+
+
 
     ##POSSIBLY DEPRECIATED
     def getSnapPos(self, arg_pos):
@@ -93,11 +182,15 @@ class Example(wx.Frame):
         self.newPos = self.getSnapPos(event.GetCoords())
         coords = (self.pos , self.newPos)
         print coords
-        self.Canvas.AddSmoothArrowLine(coords, LineWidth=2, LineColor='BLUE', ArrowHeadSize=16)
+        arrowline = FloatCanvas.SmoothArrowLine(coords, LineWidth=2, LineColor='BLUE', ArrowHeadSize=16)
+        self.Canvas.AddObject(arrowline)
+        #self.Canvas.AddSmoothArrowLine(coords, LineWidth=2, LineColor='BLUE', ArrowHeadSize=16)
         self.Canvas.Draw()
         self.Canvas.InitAll()
         for line in self.lines:
-            self.Canvas.AddSmoothArrowLine(line, LineWidth=2, LineColor="RED", ArrowHeadSize=16)
+            le_arrowline = FloatCanvas.SmoothArrowLine(line, LineWidth=2, LineColor='BLUE', ArrowHeadSize=16)
+            self.Canvas.AddObject(le_arrowline)
+            #self.Canvas.AddSmoothArrowLine(line, LineWidth=2, LineColor="RED", ArrowHeadSize=16)
         for nodes in self.node_pos_list:
             self.Canvas.AddCircle(nodes, 10, LineWidth=1, LineColor='BLACK', FillColor='BLACK')
 
